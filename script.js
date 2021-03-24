dcbadfec1ba3e0cf47d3983c6b030aa1b927eec9
Askhabov-HM <askhamzat@mail.ru>
2021-03-25 01:40:07 +0300

    готовы модальные окна. формы. исправлан слайдер. Нужно исправить текс и тайтлы

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
     // OWL SLIDER
     $(".owl-carousel").owlCarousel({
         items: 3,
-        // itemsDesktopSmall : [980,9],
-        // itemsTablet: [768,5],
-        // itemsTabletSmall: false,
-        // itemsMobile : [479,4],
         loop: true,
         nav: true,
         navText: ["<div class='owl-prev-arrow'> <div class='arrow-wrap'><img src='./svg/owl-prev-arrow.svg' alt='' class='arrow-left'></div> </div>" , "<div class='owl-next-arrow'>  <div class='arrow-wrap'><img src='./svg/owl-next-arrow.svg' alt='' class='arrow-right'></div></div>" ],
@@ -20,8 +16,8 @@ $(document).ready(function(){
         checkVisibility: false,
         responsiveBaseWidth: '.slider_row',
         responsiveClass:true,
-        animateOut: 'slideOutUp',
-        animateIn: 'slideInUp',
+        animateOut: 'fadeOutLeft',
+        animateIn: 'fadeInLeft"',
         responsive:{
             0:{
                 items: 1.5,
@@ -92,8 +88,11 @@ $(document).ready(function(){
 
     $('.phone-mask').mask('+7 (000) 000-00-00');
 
-    // END LIBS
+    //////////////////////////////////////
+    //////////// END LIBS
+    /////////////////////////////////////
 
+    // CSS ANIMATIONS
     $('.nav_btn').click( function(){
         $(this).find('.nav_btn-item').toggleClass('animate-nav-item');
         $('.sidebar').toggleClass('animate-sidebar');
@@ -102,6 +101,8 @@ $(document).ready(function(){
     $('.faq_item').click( function(){
         $(this).find('.faq_img-block').toggleClass('animate-faq_item');
     })
+
+    // MODALS
 
     $('.openModal').click( function(e){
         let self = e.target;
@@ -112,6 +113,60 @@ $(document).ready(function(){
         $('.modal_title').text(dataTitle);
         $('.modal_desc').text(dataDesc)
         $('.submitBtn').text(dataBtnText)
+    })
+
+    // GENERAL FORM SUBMIT
+
+    $('.form-general, .modalForm').submit( function(event){
+        event.preventDefault()
+        let self = event.target;
+        let data = $(self).serialize();
+        
+        $.get('https://jsonplaceholder.typicode.com/todos/1', data, function(){
+            if($('#modal').hasClass('show')){
+                // $('.loader').removeClass('d-none')
+                // $('.loader').addClass('d-flex');
+            }   
+        })
+        .done( function(response){
+            if($('#modal').hasClass('show')){
+                console.log(123)
+                $('#modal').modal('hide');
+                $('#modalSuccess').modal('show');
+            }
+            console.log(response);
+            console.log('done')
+        })
+        .fail( function(error){
+            console.log(error.responseText)
+        })
+    })
+
+    // TEAM ACTIVE ITEM APPEND STYLES 
+
+    $('.team_left-item').click( function(){
+        addRemoveTeamClass(this)
+    })
+
+    function addRemoveTeamClass(target){ // target - целевой элемент, к которому добавить .activeTeamItem
+        $('.team_left-item').each( function(index){ // перебрать все элементы с .team_left-item
+            $(this).removeClass('activeTeamItem');  // удалить .activeTeamItem
+        })
+        $(target).addClass('activeTeamItem'); // присвоить целевому элементу .activeTeamItem
+    }
+
+    // SLICK ARROW 
+    
+    $('.slick-arrow').click( function(){ // при клике на маленькую стрелку слайдера
+        let activeSlideIndex = $('.slick-active').data('slick-index'); // получить номер активного из data-slisk-index
+        let targetTeamItem = $('.team_left-item').eq(activeSlideIndex); // получить teamItem, который равен активному главному слайду
+        addRemoveTeamClass(targetTeamItem) // передать в функцию
+    })
+
+    // FAQ COLLAPSE 
+
+    $('.faq_item-link').click(function(e){
+        $(this).find('.faq_item-title').toggleClass('faq-active-item')
     })
 
   });
